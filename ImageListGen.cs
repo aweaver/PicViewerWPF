2ef6b12eeda8196e3ec37a0cd7320f16307f535f
aweaver <aaron.weaver@verizon.net>
2012-08-10 13:28:51 -0400

    First Commit

---
+++ ImageListGen.cs
@@ -0,0 +1,194 @@
+﻿#region File Description
+//-----------------------------------------------------------------------------
+// ImageListGen.cs
+//
+// Generates a list of pictures 
+// 
+// 
+// 
+//-----------------------------------------------------------------------------
+#endregion
+
+#region Using Statements
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Diagnostics;
+#endregion
+
+namespace PicViewerWPF
+{
+    class ImageListGen
+    {
+        private int picsPerDir;
+        private DirectoryInfo dir;
+        private Random rand;
+        private Dictionary<string, int> dupesTestDict;
+
+        /// <summary>
+        /// ImageListGen
+        /// </summary>
+        public ImageListGen()
+        {
+            rand = new Random();
+            dupesTestDict = new Dictionary<string, int>();
+        }
+
+        /// <summary>
+        /// generateImageList- generates a list of pictures
+        /// </summary>
+        /// <param name="srcDir">Directory to get the pictures from</param>
+        /// <param name="maxImageLstLen">the set size</param>
+        /// <returns></returns>
+        public List<string> generateImageList(string srcDir, int maxImageLstLen)
+       {
+           DirectoryInfo[] subdirs;
+
+           dir = new DirectoryInfo(srcDir);
+           subdirs = dir.GetDirectories();
+            
+            /*
+           foreach (DirectoryInfo subdir in subdirs)
+           {
+               Debug.WriteLine( "Subdir Full Name: " +subdir.FullName  );
+               Debug.WriteLine("Subdir Name: " + subdir.Name);
+           }
+             */
+
+           return (makeFileList(dir, subdirs, maxImageLstLen));
+       }
+        
+        /// <summary>
+        /// makeFileList- actually makes the list
+        /// </summary>
+        /// <param name="srcdir"></param>
+        /// <param name="subdirs"></param>
+        /// <param name="iMax"></param>
+        /// <returns></returns>
+       private List<string> makeFileList( DirectoryInfo srcdir,DirectoryInfo[] subdirs,int iMax)
+       {
+           FileInfo[] filesInfo;
+           List<string> filelist;
+           List<string> outfilelist;
+
+           filelist = new List<string>();
+           outfilelist = new List<string>();
+           
+           //check top level dir
+           //parent dir may have files besides subdirs
+          filesInfo= srcdir.GetFiles();
+          foreach (FileInfo fileinfo in filesInfo)
+          {
+              //Debug.WriteLine("file Full Name: " + fileinfo.FullName);
+              //Debug.WriteLine("file Name: " + fileinfo.Name);
+
+              if (checkFiles(fileinfo.FullName) == true)
+                  filelist.Add(fileinfo.FullName);
+          }
+
+           foreach (DirectoryInfo subdir in subdirs)
+           {
+               filesInfo = subdir.GetFiles();
+               //Debug.WriteLine("_________________________");
+               foreach (FileInfo fileinfo in filesInfo)
+               {
+                   //Debug.WriteLine("file Full Name: " + fileinfo.FullName);
+                   //Debug.WriteLine("file Name: " + fileinfo.Name);
+
+                   if (checkFiles(fileinfo.FullName)==true)
+                        filelist.Add(fileinfo.FullName);
+               }
+
+               dupesTestDict.Clear();//reset dupes dir
+
+               //Debug.WriteLine("_________________________");
+           }
+
+           outfilelist = pickFiles(filelist, iMax);
+           return (outfilelist);
+   }
+        
+        /// <summary>
+       /// checkFiles- have to check the file types because the windows image viewer control will crash if its a
+       /// non image file.
+        /// </summary>
+        /// <param name="testFilename"></param>
+        /// <returns></returns>
+       private bool checkFiles(string testFilename)
+       {
+           string stemp;
+           stemp = testFilename.ToLower();
+           if (stemp.Contains(".swf") == true)
+               return (false);
+           else if (stemp.Contains(".mp4") == true)
+               return (false);
+            else if (stemp.Contains(".html") == true)
+               return (false);
+           else if (stemp.Contains(".db") == true)
+               return (false);
+           else if (stemp.Contains(".txt") == true)
+               return (false);
+           else //check for dupes, will not work for renamed dupes
+           {
+               //Dupe finding is a fail
+               // 2 files can have the same name and not be dupes
+               // 2 files can have the same name and be dupes
+               // 2 files can have the same content but one could be bigger or have a border.
+              // if (dupesTestDict.Count == 0 || dupesTestDict.ContainsKey(stemp) == false)//not in list, save it
+              // {
+                //   dupesTestDict.Add(stemp, 1);
+                   return (true);//not in list, not a dupe
+               //}
+              // else
+              // {
+                //   return (false);//found in list already, is a dupe
+               //}
+           }
+       }
+
+        /// <summary>
+       /// pickFiles- goes through the list and picks out a set.
+        /// </summary>
+        /// <param name="filelist"></param>
+        /// <param name="iMax"></param>
+        /// <returns></returns>
+       private List<string> pickFiles(List<string> filelist, int iMax)
+       {
+           List<string> outfilelist;
+           int a;
+           int index;
+           int actualMax;// sometimes there is not enough in the dirs to cover the max.
+           outfilelist = new List<string>();
+           bool bRand;
+
+           //if there are not enough to go in a set, just read them all
+           // good for comics
+           if (filelist.Count < iMax)
+           {
+               actualMax = filelist.Count;//less files than the max requested go with lower number
+               for (a = 0; a < actualMax; a++)
+               {
+                   outfilelist.Add(filelist[a]);
+               }
+           }
+           else
+           {// plenty of content, get a set's worth
+               actualMax = iMax;
+               for (a = 0; a < actualMax; a++)
+               {
+                   index = rand.Next(filelist.Count);
+
+                   outfilelist.Add(filelist[index]);
+                   filelist.RemoveAt(index);//remove it so we do not see it again at the same place
+               }
+           }
+
+           return (outfilelist);
+       }
+
+
+
+    }
+}
---
+++ Window1.xaml.cs
@@ -0,0 +1,249 @@
+﻿#region File Description
+//-----------------------------------------------------------------------------
+// Window1.xaml.cs
+//
+// handles the main window and messages
+// 
+// 
+// 
+//-----------------------------------------------------------------------------
+#endregion
+
+#region Using Statements
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+using Microsoft.Win32;
+using System.IO;
+using System.Windows.Forms;
+#endregion
+
+namespace PicViewerWPF
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        private int ImageListlen=100;//this is what is possible
+        private int ImageListlenMax = 100;//this is what I want
+        private int currIndx=0;
+        private string[] imagelist;
+        private string imageDir = @"f:\\artref\\pony\\";//f:/artref/pony/ #
+       // d:/desktop/downloads/recro/anthro/strangem/set1/
+       // d:/desktop/downloads/recro/anthro/futam/futa series/
+        //d:\desktop\downloads\recro\anthro\strangem\set1\
+        private double dScale=1.0;
+
+        private ImageListGen imageListGen;// makes the list of images
+        List<string> filelist;// list of files to use
+        int pixelwidth, pixelheight;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Window1()
+        {
+           
+            InitializeComponent();
+            ImageListlen= ImageListlenMax;
+            imageListGen = new ImageListGen();
+            filelist = imageListGen.generateImageList(imageDir, ImageListlenMax);
+            ImageListlen = filelist.Count;
+
+            startViewingList();
+        }
+
+        /// <summary>
+        /// OnKeyDownHandler -Handles arrow keys
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnKeyDownHandler(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            //go back through picture list
+            if (e.Key == Key.Left)
+            {
+                //Debug.WriteLine("Move left ");
+                moveLeft();
+            }
+
+            //go forwards through image list
+            if (e.Key == Key.Right)
+            {
+                //Debug.WriteLine("Move right ");
+                moveRight();
+            }
+
+        }
+
+        /// <summary>
+        /// BackBtn_Click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BackBtn_Click(object sender, RoutedEventArgs e)
+        {
+            moveLeft();
+        }
+
+        /// <summary>
+        /// Forward_Click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Forward_Click(object sender, RoutedEventArgs e)
+        {
+            moveRight();
+        }
+
+        /// <summary>
+        ///  moveRight - moves forward through the list of pictures.
+        /// </summary>
+        private void moveRight()
+        {
+            if (filelist.Count == 0)
+                return;
+
+            currIndx++;
+            if (currIndx >= ImageListlen)
+                currIndx = 0;
+
+            //ImageDisp.Source = new BitmapImage(new Uri(filelist[currIndx]));
+            updateDispSource(currIndx);
+        }
+
+        /// <summary>
+        /// moveLeft - moves backwards through the list
+        /// </summary>
+        private void moveLeft()
+        {
+            if (filelist.Count == 0)
+                return;
+
+            currIndx--;
+
+            if (currIndx < 0)
+                currIndx = ImageListlen - 1;
+
+           // ImageDisp.Source = new BitmapImage(new Uri(filelist[currIndx]));
+            updateDispSource(currIndx);
+        }
+
+        /// <summary>
+        /// New_Click-  makes a new list
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void New_Click(object sender, RoutedEventArgs e)
+        {
+            startViewingList();
+        }
+
+        /// <summary>
+        /// startViewingList- makes a new viewing list
+        /// </summary>
+        private void startViewingList()
+        {
+            filelist = imageListGen.generateImageList(imageDir, ImageListlenMax);
+            currIndx = 0;
+            ImageListlen = filelist.Count;
+
+            if (filelist.Count>0)
+                updateDispSource(0);
+            //  ImageDisp.Source = new BitmapImage(new Uri(filelist[0]));//start with a picture
+        }
+
+        /// <summary>
+        /// updateDispSource- load the bitmap and send it to the imagedisp control to display it.
+        /// </summary>
+        /// <remarks > Later on we may have to save the bitmap</remarks>
+        /// <param name="index"></param>
+        private void updateDispSource(int index)
+        {
+            BitmapImage bmpImage;
+
+            PicLabel.Content = filelist[index] + " " + index.ToString();
+            bmpImage = new BitmapImage(new Uri(filelist[index]));
+            ImageDisp.Source = bmpImage;
+
+            pixelwidth = bmpImage.PixelWidth;
+            pixelheight = bmpImage.PixelHeight;
+            
+            Debug.WriteLine("W "+bmpImage.PixelWidth+" H " + bmpImage.PixelHeight);
+            dScale = 1;
+            ImageDisp.RenderTransform = new ScaleTransform(dScale, dScale, (double)(pixelwidth / 2.0f), (double)(pixelheight / 2.0f));
+        }
+
+        /// <summary>
+        /// SetDir_Click- Gets the new dir and gets the new viewing set from it.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SetDir_Click(object sender, RoutedEventArgs e)
+        {
+            // Displays an OpenFileDialog so the user can select a Cursor.
+            FolderBrowserDialog pickFoldersDialog = new FolderBrowserDialog();
+
+            pickFoldersDialog.ShowNewFolderButton = false;
+            //pickFoldersDialog.SelectedPath = imageDir;
+            pickFoldersDialog.RootFolder = System.Environment.SpecialFolder.MyComputer;//System.Environment.SpecialFolder.MyComputer
+
+            DialogResult result = pickFoldersDialog.ShowDialog();
+            // Show the Dialog.
+            // If the user clicked OK in the dialog and
+            // a .CUR file was selected, open it.
+            if (result.ToString() == "OK")
+            {
+
+                //Debug.WriteLine("Got dir " + pickFoldersDialog.SelectedPath);
+                imageDir = pickFoldersDialog.SelectedPath;
+                startViewingList();
+            }
+     }
+
+        protected override void OnPreviewMouseWheel(MouseWheelEventArgs args)
+        {
+            base.OnPreviewMouseWheel(args);
+            Transform temptransform;
+   
+           // Debug.WriteLine("Mouse wheel " + args.Delta);
+
+           // ImageDisp.LayoutTransform=new ScaleTransform(2.0,2.0) ;
+
+           
+            if (args.Delta > 0)
+               dScale+=0.10;
+
+            if (args.Delta < 0)
+            {
+                dScale -= 0.10;
+                if (dScale < 1.0)
+                    dScale = 1.0;
+            }
+                
+             //Debug.WriteLine(" dScale " +dScale);
+             //Debug.WriteLine(" ImageDisp.LayoutTransform " + ImageDisp.LayoutTransform);
+             ImageDisp.RenderTransform = new ScaleTransform(dScale, dScale,(double)(pixelwidth / 2.0f), (double)(pixelheight / 2.0f));
+            
+           
+           // ImageDisp.InvalidateMeasure();
+            //ImageDisp.InvalidateVisual();
+            //ImageDisp.
+        }
+
+
+    }
+}
